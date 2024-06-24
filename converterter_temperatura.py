701b50426cde1ce5634559c62c6b743fd0e373ef
gustavo <gugasalles.g@gmail.com>
2024-06-24 20:09:32 -0300

    teste removido

--- converterter_temperatura.py
+++ converterter_temperatura.py
@@ -1,13 +1,9 @@
 class ConverterTemperatura:
     def __init__():
         pass
-    
+
     def celsius_to_fahrenheit(celsius_value):
         return celsius_value * (9/5) + 32
 
     def fahrenheit_to_celsius(fah_value):
         return (fah_value - 32) / (9/5)
-
-# Testing Area
-result = ConverterTemperatura.fahrenheit_to_celsius(32)
-print('Result:', result)
