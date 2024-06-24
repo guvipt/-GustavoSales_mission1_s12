1953a60994eff75f2ffc968e17887a7126a5308a
gustavo <gugasalles.g@gmail.com>
2024-06-24 20:08:02 -0300

    inplementação celsius_to_fahrenheit

--- converterter_temperatura.py
+++ converterter_temperatura.py
@@ -1,9 +1,9 @@
 class ConverterTemperatura:
     def __init__():
         pass
-
-    def celsius_to_fahrenheit():
-        pass
+    
+    def celsius_to_fahrenheit(celsius_value):
+        return celsius_value * (9/5) + 32
 
     def fahrenheit_to_celsius(fah_value):
         return (fah_value - 32) / (9/5)
